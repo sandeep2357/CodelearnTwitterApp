209fdb2f04ba9f2d1de3c18bc4f9a70fcd51cf7f
Sandeep Bypina <kumarsandeep2357@gmail.com>
2015-09-22 05:08:00 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/sandeep/codelearntwitterapp/TweetListActivity.java
@@ -0,0 +1,17 @@
+package com.example.sandeep.codelearntwitterapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class TweetListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_list);
+    }
+
+
+}
